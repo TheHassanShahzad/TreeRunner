4a3bee2b7759c86a04d2342d7109281d266e52c7
Hassan Shahzad <hshahzad2005108277@gmail.com>
2024-05-18 21:25:25 +0100

    updated homing code

--- code/test_code/homing_test/homing_test.ino
+++ code/test_code/homing_test/homing_test.ino
@@ -1,14 +1,13 @@
 // Define motor control pins
-const int motorPin1 = 10; // Motor control pin 1
-const int motorPin2 = 11; // Motor control pin 2
-const int enablePin = 9;  // Motor enable pin
+const int motorPin1 = 1; // Motor control pin 1
+const int motorPin2 = 2; // Motor control pin 2
 
 // Define the switch pin
 const int switchPin = 3;
 
 // Define the encoder pins
-const int encoderPinA = 2; // Encoder channel A
-const int encoderPinB = 4; // Encoder channel B
+const int encoderPinA = 0; // Encoder channel A
+const int encoderPinB = 10; // Encoder channel B
 
 // Variables for the encoder counts
 volatile long encoderCounts = 0;
@@ -18,7 +17,6 @@ void setup() {
   // Initialize motor control pins
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
-  pinMode(enablePin, OUTPUT);
 
   // Initialize switch pin with internal pull-up resistor
   pinMode(switchPin, INPUT_PULLUP);
@@ -63,19 +61,16 @@ void loop() {
 void startMotorForward() {
   digitalWrite(motorPin1, HIGH);
   digitalWrite(motorPin2, LOW);
-  analogWrite(enablePin, 255); // Set speed to maximum
 }
 
 void startMotorBackward() {
   digitalWrite(motorPin1, LOW);
   digitalWrite(motorPin2, HIGH);
-  analogWrite(enablePin, 255); // Set speed to maximum
 }
 
 void stopMotor() {
   digitalWrite(motorPin1, LOW);
   digitalWrite(motorPin2, LOW);
-  analogWrite(enablePin, 0); // Turn off motor
 }
 
 void encoderAChange() {
